950cbb8ac1b7d4be88454bd650c4f8ec1ad7612f
mgonzalesips <mgonzales@ips.edu.ar>
2026-06-09 15:26:37 -0300

    ejemplos

--- ClinicaMedica/Program.cs
+++ ClinicaMedica/Program.cs
@@ -9,6 +9,7 @@ namespace ClinicaMedica
         {
             var context = new ClinicaContext();
 
+            //seleccionar y cargar todos los turnos y sus relaciones
             var turnos = context.Turnos
                 .Include(t => t.Paciente)
                 .Include(t => t.Medico)
@@ -22,6 +23,16 @@ namespace ClinicaMedica
             {
                 Console.WriteLine($"{t.Fecha} {t.Hora} | {t.Paciente.Nombre} {t.Paciente.Apellido} | {t.Medico.Nombre} {t.Medico.Apellido} | {t.Especialidad.Nombre} | {t.Estado.Descripcion}");
             }
+
+            var turnos2 = context.Turnos.Where(t => t.Estado.Descripcion == "cancelado");
+            foreach (var turno in turnos2)
+            {
+                
+                Console.WriteLine($"{turno.Estado.Descripcion} | {turno.Especialidad.Nombre} | {turno.Paciente.Nombre}");
+            }
+
+            Paciente paciente = context.Pacientes.FirstOrDefault(p => p.Dni == 27999000);
+            Console.WriteLine($"{paciente.Nombre} {paciente.Apellido}");
         }
     }
 }
